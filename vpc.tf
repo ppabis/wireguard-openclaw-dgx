6747d62b9a731daf226e4a2493720ae318f0ea1c
ppabis <piotr@pabis.eu>
2026-04-14 19:33:00 +0200

    Setup the VPC

---
+++ vpc.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.6.0"
+
+  name = "my-vpc"
+  cidr = "10.189.80.0/21"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
+  public_subnets  = ["10.189.80.0/24", "10.189.81.0/24", "10.189.82.0/24"]
+  private_subnets = ["10.189.83.0/24", "10.189.84.0/24", "10.189.85.0/24"]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  enable_nat_gateway   = false
+
+  enable_ipv6                                    = true
+  public_subnet_assign_ipv6_address_on_creation  = true
+  private_subnet_assign_ipv6_address_on_creation = true
+  public_subnet_ipv6_prefixes                    = [0, 1, 2]
+  private_subnet_ipv6_prefixes                   = [3, 4, 5]
+  
+  private_subnet_tags = { type = "private" }
+  public_subnet_tags  = { type = "public" }
+}
